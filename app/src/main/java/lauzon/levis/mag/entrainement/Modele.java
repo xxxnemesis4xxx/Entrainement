f4d8407e6c3b248af02b119f7310ddbbebb8a070
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-01 18:04:38 -0500

    View for Modele + button redirecting to it on click

--- app/src/main/java/lauzon/levis/mag/entrainement/Modele.java
+++ app/src/main/java/lauzon/levis/mag/entrainement/Modele.java
@@ -1,26 +1,24 @@
 package lauzon.levis.mag.entrainement;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends Activity {
+public class Modele extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_modele);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_modele, menu);
         return true;
     }
 
@@ -37,10 +35,5 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void chandeDisplayToModele(View view) {
-        Intent intent = new Intent(this, Modele.class);
-        startActivity(intent);
     }
 }
