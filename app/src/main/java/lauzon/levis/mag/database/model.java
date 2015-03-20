a497506270612bc00acc8ff15bc7915b43578933
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-20 14:32:44 -0400

    Created model class to represent data inside DB

---
+++ app/src/main/java/lauzon/levis/mag/database/model.java
@@ -0,0 +1,28 @@
+package lauzon.levis.mag.database;
+
+
+public class model {
+    private long id;
+    private String nom;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return nom;
+    }
+}
