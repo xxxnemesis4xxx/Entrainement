284753e148eefe551e117e8675958ffae7ec6686
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-20 11:39:25 -0400

    Database Setup

---
+++ app/src/main/java/lauzon/levis/mag/database/MySQLiteHelper.java
@@ -0,0 +1,42 @@
+package lauzon.levis.mag.database;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class MySQLiteHelper extends SQLiteOpenHelper {
+
+    public static final String TABLE_EXERCICE = "exercice";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NOM = "nom";
+
+    private static final String DATABASE_NAME = "entrainement.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Database creation sql statement
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_EXERCICE + "(" + COLUMN_ID
+            + " integer primary key autoincrement, " + COLUMN_NOM
+            + " text not null);";
+
+    public MySQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(MySQLiteHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS);
+        onCreate(db);
+    }
+
+}
