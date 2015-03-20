f6b9a2b9bd968cdea0ada9e6208cd28b238be6db
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-20 18:07:25 -0400

    Display List of Models on screen

--- app/src/main/java/lauzon/levis/mag/Models/ViewModels.java
+++ app/src/main/java/lauzon/levis/mag/Models/ViewModels.java
@@ -1,25 +1,43 @@
 package lauzon.levis.mag.Models;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 import lauzon.levis.mag.database.EntrainementDatasource;
+import lauzon.levis.mag.database.model;
 import lauzon.levis.mag.entrainement.R;
 
 public class ViewModels extends Activity {
     private EntrainementDatasource datasource;
-
+    private int previousId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_models);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
 
+        //Get our Database
         datasource = new EntrainementDatasource(this);
         datasource.open();
+
+        //Get out List of Models
+        List<model> values = datasource.getAllModels();
+
+        for (int i = 0; i < values.size(); i++) {
+            LoadModelsLayout(values.get(i));
+            previousId = (int)values.get(i).getId();
+        }
     }
 
 
@@ -43,5 +61,43 @@ public class ViewModels extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void LoadModelsLayout(model value) {
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams( RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT );
+        RelativeLayout.LayoutParams params2 = new RelativeLayout.LayoutParams( RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT );
+        RelativeLayout layout = (RelativeLayout)findViewById(R.id.relativeLayout);
+
+        TextView tv = new TextView(this);
+        tv.setText(value.getNom());
+        tv.setLayoutParams(params);
+        tv.setTextSize(20);
+        tv.setId((int) value.getId());
+        tv.setHeight(90);
+        params.setMargins(0,20,0,0);
+
+        if (previousId > 0) {
+            params.addRule(RelativeLayout.BELOW, previousId);
+        }
+
+        Button button = new Button(this);
+        button.setText("Voir les exercices");
+        button.setLayoutParams(params2);
+        button.setHeight(20);
+
+        params2.addRule(RelativeLayout.RIGHT_OF, tv.getId());
+        params2.addRule(RelativeLayout.BELOW, previousId);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Toast toast = Toast.makeText(context,"OUI!",Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        layout.addView(tv);
+        layout.addView(button);
     }
 }
