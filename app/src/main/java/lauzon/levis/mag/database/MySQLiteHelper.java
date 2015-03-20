25f0f7e18a3f5ce04b0e55ba8a0daf0d3fdff181
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-20 14:26:53 -0400

    New Table!

--- app/src/main/java/lauzon/levis/mag/database/MySQLiteHelper.java
+++ app/src/main/java/lauzon/levis/mag/database/MySQLiteHelper.java
@@ -11,15 +11,24 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
     public static final String TABLE_EXERCICE = "exercice";
     public static final String COLUMN_ID = "_id";
     public static final String COLUMN_NOM = "nom";
-
+    public static final String COLUMN_REF_MODEL = "idmodel";
+    public static final String TABLE_MODEL = "model";
+    public static final String COLUMN_ID_MODEL = "_id";
+    public static final String COLUMN_NOM_MODEL = "nom";
     private static final String DATABASE_NAME = "entrainement.db";
     private static final int DATABASE_VERSION = 1;
 
     // Database creation sql statement
-    private static final String DATABASE_CREATE = "create table "
-            + TABLE_EXERCICE + "(" + COLUMN_ID
+    private static final String CREATE_TABLE_MODEL = "create table "
+            + TABLE_MODEL + "(" + COLUMN_ID_MODEL
             + " integer primary key autoincrement, " + COLUMN_NOM
             + " text not null);";
+
+    private static final String CREATE_TABLE_EXERCICE = "create table "
+            + TABLE_EXERCICE + "(" + COLUMN_ID
+            + " integer primary key autoincrement, " + COLUMN_NOM
+            + " text not null, " + COLUMN_REF_MODEL + " integer, FOREIGN KEY ("
+            + COLUMN_REF_MODEL +") REFERENCES " + TABLE_MODEL + " ("+ COLUMN_ID +"));";
 
     public MySQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -27,7 +36,8 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase database) {
-        database.execSQL(DATABASE_CREATE);
+       database.execSQL(CREATE_TABLE_MODEL);
+        database.execSQL(CREATE_TABLE_EXERCICE);
     }
 
     @Override
@@ -36,6 +46,7 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
                 "Upgrading database from version " + oldVersion + " to "
                         + newVersion + ", which will destroy all old data");
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_EXERCICE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MODEL);
         onCreate(db);
     }
 
