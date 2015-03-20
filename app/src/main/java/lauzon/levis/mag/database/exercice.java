a805ccf9a0b82100376497655597a22f62ce1bfd
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-20 14:36:43 -0400

    Updated Exercice Table

--- app/src/main/java/lauzon/levis/mag/database/exercice.java
+++ app/src/main/java/lauzon/levis/mag/database/exercice.java
@@ -4,6 +4,7 @@ package lauzon.levis.mag.database;
 public class exercice {
     private long id;
     private String nom;
+    private long refidmodel;
 
     public long getId() {
         return id;
@@ -19,6 +20,14 @@ public class exercice {
 
     public void setNom(String nom) {
         this.nom = nom;
+    }
+
+    public long getRefidmodel() {
+        return refidmodel;
+    }
+
+    public void setRefidmodel(long refidmodel) {
+        this.refidmodel = refidmodel;
     }
 
     // Will be used by the ArrayAdapter in the ListView
