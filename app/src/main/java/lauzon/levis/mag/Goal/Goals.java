ec422cf4bc10a09f0255baa2f2149b4826a81a85
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-22 02:19:55 -0400

    Return button

--- app/src/main/java/lauzon/levis/mag/Goal/Goals.java
+++ app/src/main/java/lauzon/levis/mag/Goal/Goals.java
@@ -1,15 +1,24 @@
 package lauzon.levis.mag.Goal;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
+import java.util.List;
+
+import lauzon.levis.mag.Models.ModelPanel;
+import lauzon.levis.mag.database.EntrainementDatasource;
+import lauzon.levis.mag.database.exercice;
 import lauzon.levis.mag.entrainement.R;
 
 
 public class Goals extends Activity {
-
+    private EntrainementDatasource datasource;
+    private long idExercice = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +27,21 @@ public class Goals extends Activity {
         Bundle bundle = getIntent().getExtras();
         long id = bundle.getLong("ID");
 
+        //Get our Database
+        datasource = new EntrainementDatasource(this);
+        datasource.open();
 
+        //Get out List of Models
+        exercice value = datasource.getExercice(id);
+        idExercice = value.getId();
+
+        if (value.getGoal() == null || value.getGoal().isEmpty()) {
+            EditText et = (EditText)findViewById(R.id.etGoal);
+            et.setText("Vous avez aucun objectif pour cet exercice");
+        } else {
+            EditText et = (EditText)findViewById(R.id.etGoal);
+            et.setText(value.getGoal());
+        }
     }
 
 
@@ -42,5 +65,9 @@ public class Goals extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void closeCurrentIntent(View view) {
+        finish();
     }
 }
