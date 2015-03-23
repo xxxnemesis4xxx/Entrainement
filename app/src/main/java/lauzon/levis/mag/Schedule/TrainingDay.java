fc9d46998a393636111bc002a304419faf6c8a70
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-23 11:23:26 -0400

    Show the list of Model

--- app/src/main/java/lauzon/levis/mag/Schedule/TrainingDay.java
+++ app/src/main/java/lauzon/levis/mag/Schedule/TrainingDay.java
@@ -5,15 +5,45 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import java.util.List;
+
+import lauzon.levis.mag.database.EntrainementDatasource;
+import lauzon.levis.mag.database.model;
 import lauzon.levis.mag.entrainement.R;
 
 public class TrainingDay extends Activity {
+    private EntrainementDatasource datasource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training_day);
+
+        datasource = new EntrainementDatasource(this);
+        datasource.open();
+
+        List<model> values = datasource.getAllModels();
+
+        ArrayAdapter<model> adapter = new ArrayAdapter<model>(this,
+                android.R.layout.simple_list_item_1, values);
+
+        final ListView list = (ListView)findViewById(R.id.listView);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                Object o = list.getItemAtPosition(position);
+                model str=(model)o;//As you are using Default String Adapter
+                Toast.makeText(getBaseContext(), ((model) o).getNom(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
