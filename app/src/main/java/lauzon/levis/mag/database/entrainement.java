9b126a2557baa00d6f9f18deb2cbdfc6ba4b6a3c
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-23 16:52:30 -0400

    entrainement model

---
+++ app/src/main/java/lauzon/levis/mag/database/entrainement.java
@@ -0,0 +1,51 @@
+package lauzon.levis.mag.database;
+
+import java.sql.Date;
+
+public class entrainement {
+    private long id;
+    private Date date;
+    private int rating;
+    private String infosupp;
+    private long refidmodel;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getRefidmodel() {
+        return refidmodel;
+    }
+
+    public void setRefidmodel(long refidmodel) {
+        this.refidmodel = refidmodel;
+    }
+
+    public String getInfosupp() {
+        return infosupp;
+    }
+
+    public void setInfosupp(String infosupp) {
+        this.infosupp = infosupp;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
